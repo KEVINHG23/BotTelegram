4eb922dcb8df17784aa153ba7714eacf6e2cbf56
kevin <kevinsalvadorh@gmail.com>
2026-02-21 22:32:51 -0600

    Bot Telegram

--- bot.py
+++ bot.py
@@ -16,15 +16,26 @@ async def manejar(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if user_id != ADMIN_ID:
         return
 
+    # ABRIR GRUPO
     if texto in ["buenos dias", "buenas tardes", "buenas noches"]:
         permisos = ChatPermissions(can_send_messages=True)
         await context.bot.set_chat_permissions(chat_id, permisos)
         await update.message.reply_text("Grupo abierto.")
 
-    elif texto == "gracias por su atencion":
+    # CERRAR GRUPO
+    elif texto == "grupo cerrado":
+        # Bloquear a todos
         permisos = ChatPermissions(can_send_messages=False)
         await context.bot.set_chat_permissions(chat_id, permisos)
-        await update.message.reply_text("Grupo cerrado.")
+
+        # Permitir solo al admin enviar mensajes
+        await context.bot.restrict_chat_member(
+            chat_id=chat_id,
+            user_id=ADMIN_ID,
+            permissions=ChatPermissions(can_send_messages=True)
+        )
+
+        await update.message.reply_text("Grupo cerrado. Solo el administrador puede escribir.")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, manejar))
